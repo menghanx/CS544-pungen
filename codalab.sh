0b494025ac2bc32161d942e7fdd86c1a53dd4e17
IvyDLiu <hirodddd@gmail.com>
2021-11-24 16:05:37 -0800

    rename nltk.py

--- codalab.sh
+++ codalab.sh
@@ -9,7 +9,8 @@ python -m pungen.retriever --doc-file /tmp/sent.tokenized.txt --path retriever.p
 # cp /tmp/models/sgns-e15.pt ./skipgram/model.pt; cp /tmp/skipgram/dict.txt ./skipgram/dict.txt
 
 for split in train valid; do PYTHONPATH=. python scripts/make_src_tgt_files.py -i data/$split.txt -o /tmp/edit/$split --delete-frac 0.5 --window-size 2 --random-window-size; done; 
-python -m pungen.preprocess --source-lang src --target-lang tgt --destdir /tmp/edit/bin/data --thresholdtgt 80 --thresholdsrc 80 --validpref /tmp/edit/valid --trainpref /tmp/edit/train --workers 8; cp /tmp/edit/bin/data/*.txt ./combiner-data
+python -m pungen.preprocess --source-lang src --target-lang tgt --destdir /tmp/edit/bin/data --thresholdtgt 80 --thresholdsrc 80 --validpref /tmp/edit/valid --trainpref /tmp/edit/train --workers 8; mkdir combiner-data; cp /tmp/edit/bin/data/*.txt combiner-data;
+
 
 for split in train valid; do PYTHONPATH=. python scripts/make_src_tgt_files.py -i data/$split.txt -o /tmp/edit/$split --delete-frac 0.5 --window-size 2 --random-window-size; done; python -m pungen.preprocess --source-lang src --target-lang tgt --destdir /tmp/edit/bin/data --thresholdtgt 80 --thresholdsrc 80 --validpref /tmp/edit/valid --trainpref /tmp/edit/train --workers 8; python -m pungen.train /tmp/edit/bin/data -a lstm --source-lang src --target-lang tgt --task edit --insert deleted --combine token --criterion cross_entropy --encoder lstm --decoder-attention True --optimizer adagrad --lr 0.01 --lr-scheduler reduce_lr_on_plateau --lr-shrink 0.5 --clip-norm 5 --max-epoch 50 --max-tokens 7000 --no-epoch-checkpoints --save-dir models --no-progress-bar --log-interval 50000; rm models/checkpoint_last.pt
 
